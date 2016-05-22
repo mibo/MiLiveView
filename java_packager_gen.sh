d161138e5123e5ae2630ebda73be234ce64f9f07
mibo <mibo@mirb.de>
2016-05-22 10:02:40 +0200

    Add javapackager

---
+++ java_packager_gen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ROOT_DIR=./
+
+if [ ! -z $1 ]; then
+	ROOT_DIR=$1
+fi
+
+APP_NAME=MiLiveView
+APP_CLASS=hello.ScalaFXHelloWorld
+SRC_DIR=$ROOT_DIR/target/scala-2.11
+SRC_FILE=MiLiVi-assembly-1.0.jar
+OUT_DIR=$ROOT_DIR/target/
+OUT_FILE=app-result
+JAVA_FX_PACKAGER=javapackager
+
+echo "Create from root dir '$ROOT_DIR' into '$OUT_DIR' with name '$OUT_FILE'"
+
+#${JAVA_HOME}/bin/javapackager -createjar -nocss2bin -appclass $APP_CLASS -srcdir $SRC_DIR -outdir $OUT_DIR -outfile $OUT_FILE $JAVA_FX_PACKAFER
+
+${JAVA_HOME}/bin/javapackager -deploy -native image -name $APP_NAME -appclass $APP_CLASS -srcdir $SRC_DIR -srcfiles $SRC_FILE -outdir $OUT_DIR -outfile $OUT_FILE $JAVA_FX_PACKAFER
