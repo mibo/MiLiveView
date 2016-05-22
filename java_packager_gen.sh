2336fd37c0851295c63d250b9a5131fe84d1d175
mibo <mibo@mirb.de>
2016-05-22 18:49:14 +0200

    Removed not used parts

--- java_packager_gen.sh
+++ java_packager_gen.sh
@@ -11,10 +11,9 @@ SRC_DIR=$ROOT_DIR/target/scala-2.11
 SRC_FILE=MiLiVi-assembly-1.0.jar
 OUT_DIR=$ROOT_DIR/target/
 OUT_FILE=app-result
-JAVA_FX_PACKAGER=javapackager
 
 echo "Create from root dir '$ROOT_DIR' into '$OUT_DIR' with name '$OUT_FILE'"
 
-#${JAVA_HOME}/bin/javapackager -createjar -nocss2bin -appclass $APP_CLASS -srcdir $SRC_DIR -outdir $OUT_DIR -outfile $OUT_FILE $JAVA_FX_PACKAFER
+#${JAVA_HOME}/bin/javapackager -createjar -nocss2bin -appclass $APP_CLASS -srcdir $SRC_DIR -outdir $OUT_DIR -outfile $OUT_FILE
 
-${JAVA_HOME}/bin/javapackager -deploy -native image -name $APP_NAME -appclass $APP_CLASS -srcdir $SRC_DIR -srcfiles $SRC_FILE -outdir $OUT_DIR -outfile $OUT_FILE $JAVA_FX_PACKAFER
+${JAVA_HOME}/bin/javapackager -deploy -native image -name $APP_NAME -appclass $APP_CLASS -srcdir $SRC_DIR -srcfiles $SRC_FILE -outdir $OUT_DIR -outfile $OUT_FILE
